f92d39959b5ee3a0e73c213ffcc3ef2b0f22e7f2
Eric Baur <baurfamily@gmail.com>
2018-12-06 12:55:14 -0800

    day 4 part 1 working

--- Day03/FabricClaims-first.playground/Contents.swift
+++ Day03/FabricClaims-first.playground/Contents.swift
@@ -1,5 +1,18 @@
 import Cocoa
 
+//we assume no more than 10,000 points in a row
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+    
+    var hashValue: Int {
+        return 10000*x + y
+    }
+}
+func ==(lhs: Point, rhs: Point) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y
+}
+
 struct Swatch {
     var id: Int
     
@@ -13,7 +26,7 @@ struct Swatch {
         //ugh, gets around range issues with substring/match
         let desc = description as NSString
 
-        let regex = try! NSRegularExpression(pattern: "^#(\\d) @ (\\d+),(\\d+): (\\d+)x(\\d+)$", options: [])
+        let regex = try! NSRegularExpression(pattern: "^#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)$", options: [])
         
         let matches = regex.matches(in: description, options: [], range: NSMakeRange(0, description.count))
         let match = matches.first!
@@ -27,19 +40,45 @@ struct Swatch {
             height: Int(desc.substring(with: match.range(at: 5)))!
         )
     }
+    
+    var points: [Point] {
+        get {
+            var p: Array<Point> = []
+            
+            for i in stride(from: xPos, to: xPos+width, by: 1) {
+                for j in stride(from: yPos, to: yPos+height, by: 1) {
+                    p.append(Point(x: i, y: j))
+                }
+            }
+            return p
+        }
+    }
 }
 
-let dataUrl = Bundle.main.url(forResource: "sample", withExtension: "txt")
+let dataUrl = Bundle.main.url(forResource: "data", withExtension: "txt")
 let input = try! String(contentsOf: dataUrl!)
-
 let lines = input.components(separatedBy: .newlines)
 
 var claims: Dictionary<Int,Swatch> = [:]
+var fabric: Dictionary<Point,Int> = [:]
+
 for line in lines {
     guard line.count > 0 else { continue }
-    print("converting line: <\(line)>")
+
     let swatch = Swatch.parse(line)
     claims[swatch.id] = swatch
+    
+    for point in swatch.points {
+        fabric[point] = (fabric[point] ?? 0) + 1
+    }
 }
 
-print(claims)
+var overlapCount = 0
+for (key,value) in fabric {
+    if value > 1 {
+        overlapCount += 1
+        print("Overlap of \(value) at: \(key)")
+    }
+}
+
+print("total overlap: \(overlapCount)")
