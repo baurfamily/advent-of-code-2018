e760fb5d5d0a57e370a6e243c37417c0536a7fa5
Eric Baur <baurfamily@gmail.com>
2018-12-13 08:41:59 -0800

    working?

---
+++ Day05/Polymers-second.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+//I don't think this does quite what I want it to
+guard let polymer = getData(from: "data").first else { exit(0) }
+
+
+var smallestLetter = "A"
+var smallestPolymerCount = polymer.count
+for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+    let editedPolymer = polymer.replacingOccurrences(of: String(letter), with: "")
+    let editedCount = resolvePolarity(polymer: editedPolymer).count
+    print("letter \(letter): \(editedPolymer.count) -> \(editedCount)")
+
+    if editedCount < smallestPolymerCount {
+        smallestPolymerCount = editedCount
+        smallestLetter = String(letter)
+    }
+}
+
+print("letter: \(smallestLetter)")
+print("edited: \(smallestPolymerCount)")
---
+++ Day05/Polymers-second.playground/Sources/Polarity.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public func resolvePolarity(polymer: String) -> String {
+    var editedPolymer = polymer
+    
+    let end = editedPolymer.count
+    var i = end-1
+    while i > 0 {
+        let index0 = editedPolymer.index(editedPolymer.startIndex, offsetBy: i)
+        let index1 = editedPolymer.index(editedPolymer.startIndex, offsetBy: i-1)
+        
+        let char0 = String(editedPolymer[index0])
+        let char1 = String(editedPolymer[index1])
+        
+        guard char0.lowercased() == char1.lowercased() else { i -= 1; continue }
+        guard char0 != char1 else { i -= 1; continue }
+        
+        editedPolymer.remove(at: index0)
+        editedPolymer.remove(at: index1)
+        
+        i -= 2
+    }
+    
+    return editedPolymer
+}
